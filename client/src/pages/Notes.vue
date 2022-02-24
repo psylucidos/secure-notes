20d2dca60e56d46b9206c861a1854f7e432201b8
psylucidos <psylucidos@gmail.com>
2022-02-24 17:58:40 +1100

    Working on text editor

--- client/src/pages/Notes.vue
+++ client/src/pages/Notes.vue
@@ -10,12 +10,12 @@
           <q-item
             v-for="note in notes"
             clickable v-ripple
-            @click="tab = note.content.split('\n')[0]"
+            @click="tab = note.id"
             :key="note.id"
           >
             <q-item-section>
-              <q-item-label>{{ note.content.split('\n')[0] }}</q-item-label>
-              <q-item-label caption>{{ note.content.split('\n')[1] }}</q-item-label>
+              <q-item-label>{{ note.id }}</q-item-label>
+              <q-item-label caption>test</q-item-label>
             </q-item-section>
           </q-item>
         </q-list>
@@ -31,9 +31,14 @@
         transition-prev="jump-up"
         transition-next="jump-up"
       >
-        <q-tab-panel v-for="note in notes" :name="note.content.split('\n')[0]" :key="note.id">
-          <div class="text-h4 q-mb-md">{{ note.content.split('\n')[0] }}</div>
-          <p contenteditable="true">{{ note.content.split('\n')[1] }}</p>
+        <q-tab-panel v-for="note in notes" :name="note.id" :key="note.id">
+          <div class="text-h4 q-mb-md">{{ note.content.split('<br/>')[0] }}</div>
+          <p
+            class="editable"
+            @keydown="endEdit"
+            contenteditable="true"
+          ></p>
+          {{ note.content }}
         </q-tab-panel>
       </q-tab-panels>
     </div>
@@ -61,6 +66,41 @@
 
 <script>
 import { defineComponent } from 'vue';
+//
+// function splice(s, idx, rem, str) {
+//   return s.slice(0, idx) + str + s.slice(idx + Math.abs(rem));
+// }
+
+function parseMarkdown(markdownText) {
+  const htmlText = markdownText
+    .replace(/^### (.*$)/gim, '<h3>### $1</h3>')
+    .replace(/^## (.*$)/gim, '<h2>## $1</h2>')
+    .replace(/^# (.*$)/gim, '<h1># $1</h1>')
+    .replace(/^> (.*$)/gim, '<blockquote>$1</blockquote>')
+    .replace(/\*\*(.*)\*\*/gim, '<b>**$1**</b>')
+    .replace(/\*(.*)\*/gim, '<i>*$1*</i>')
+    .replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />")
+    .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
+    .replace(/\n$/gim, '<br />');
+
+  return htmlText.trim();
+}
+
+function getCaretIndex(element) {
+  let position = 0;
+  const isSupported = typeof window.getSelection !== 'undefined';
+  if (isSupported) {
+    const selection = window.getSelection();
+    if (selection.rangeCount !== 0) {
+      const range = window.getSelection().getRangeAt(0);
+      const preCaretRange = range.cloneRange();
+      preCaretRange.selectNodeContents(element);
+      preCaretRange.setEnd(range.endContainer, range.endOffset);
+      position = preCaretRange.toString().length;
+    }
+  }
+  return position;
+}
 
 export default defineComponent({
   name: 'PageNotes',
@@ -70,7 +110,7 @@ export default defineComponent({
       notes: [
         {
           id: 0,
-          content: '# hello\nI hate mondays.',
+          content: 'hello',
         },
         {
           id: 1,
@@ -78,6 +118,52 @@ export default defineComponent({
         },
       ],
     };
+  },
+  methods: {
+    updateNote() {
+      // const text = document.getElementById('editor');
+      // console.log('key', e.keyCode);
+      // console.log('textInput', text);
+      // console.log('content', this.notes[0].content);
+      // const index = 0;
+      // console.log('index', index);
+
+      // if (e.keyCode >= 65 && e.keyCode <= 90) {
+      //   this.notes[0].content = splice(this.notes[0].content, index, 0, e.key);
+      // } else if (e.keyCode === 8) {
+      //   console.log('DEL');
+      //   this.notes[0].content = splice(this.notes[0].content, index, 1, '');
+      // }
+      console.log('test');
+    },
+    onEdit(e) {
+      const src = e.target.innerText;
+      this.notes[0].content = src;
+
+      console.log(parseMarkdown(src));
+
+      e.target.innerHTML = parseMarkdown(src);
+    },
+    onBlur() {
+      console.log('blr');
+    },
+    endEdit(e) {
+      console.log('endingz');
+      console.log(getCaretIndex(e.target));
+      // const el = document.getElementByClass('editable');
+      // const selection = window.getSelection();
+      // const range = document.createRange();
+      // selection.removeAllRanges();
+      // range.selectNodeContents(el);
+      // range.collapse(false);
+      // selection.addRange(range);
+      // el.focus();
+      const selection = window.getSelection();
+      selection.extend(e.target, -3);
+      // this.$el.querySelector('.editable').blur();
+      // this.onEdit(e);
+      // this.$el.querySelector('.editable').focus();
+    },
   },
 });
 </script>
