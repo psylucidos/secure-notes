39f8317d9126449ca6cfa52e940ceb2fecdd0237
psylucidos <psylucidos@gmail.com>
2022-03-01 17:07:12 +1100

    Working on sketchy markdown parser, still buggy

--- client/src/pages/Notes.vue
+++ client/src/pages/Notes.vue
@@ -35,7 +35,7 @@
           <div class="text-h4 q-mb-md">{{ note.content.split('<br/>')[0] }}</div>
           <p
             id="editable"
-            @keydown="onEdit"
+            @keyup="onEdit"
             contenteditable="true"
           ></p>
           {{ note.content }}
@@ -66,24 +66,24 @@
 
 <script>
 import { defineComponent } from 'vue';
-//
-// function splice(s, idx, rem, str) {
-//   return s.slice(0, idx) + str + s.slice(idx + Math.abs(rem));
-// }
 
 function parseMarkdown(markdownText) {
   const htmlText = markdownText
-    .replace(/^### (.*$)/gim, '<h3>### $1</h3>')
-    .replace(/^## (.*$)/gim, '<h2>## $1</h2>')
-    .replace(/^# (.*$)/gim, '<h1># $1</h1>')
+    .replace(/^### (.*$)/gim, '<h5>### $1</h5>')
+    .replace(/^## (.*$)/gim, '<h4>## $1</h4>')
+    .replace(/^# (.*$)/gim, '<h3># $1</h3>')
     .replace(/^> (.*$)/gim, '<blockquote>$1</blockquote>')
     .replace(/\*\*(.*)\*\*/gim, '<b>**$1**</b>')
-    .replace(/\*(.*)\*/gim, '<i>*$1*</i>')
+    .replace(/_(.*)_/gim, '<i>_$1_</i>')
     .replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />")
-    .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
-    .replace(/\n$/gim, '<br />');
+    .replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>");
+    // .replace(/\n$/gim, '<br />');
 
-  return htmlText.trim();
+  return htmlText;
+}
+
+function parseMarkdown(text) {
+  return sd.render(text);
 }
 
 function getCaretIndex(element) {
@@ -115,12 +115,13 @@ function setCaret(elementID, position) {
     currentPos += content.length;
     // console.log('CHILD', i, content, 'position', currentPos);
     if (currentPos >= position) {
-      console.log(el.childNodes[i], currentPos, position);
+      console.log('child node', el.childNodes[i], 'current position', currentPos, 'postion?', position);
       console.log('selecting', position - passedPositions, 'of', el.childNodes[i]);
-      if (i >= 1) {
-        console.log('selecting', (position - passedPositions) - 1, 'instead');
-        range.setStart(el.childNodes[i].firstChild, position - passedPositions - 1);
+      if (el.childNodes[i].firstChild) {
+        console.log('going into first child');
+        range.setStart(el.childNodes[i].firstChild, position - passedPositions);
       } else {
+        console.log('not going into child');
         range.setStart(el.childNodes[i], position - passedPositions);
       }
       range.collapse(true);
@@ -151,65 +152,22 @@ export default defineComponent({
     };
   },
   methods: {
-    updateNote() {
-      // const text = document.getElementById('editor');
-      // console.log('key', e.keyCode);
-      // console.log('textInput', text);
-      // console.log('content', this.notes[0].content);
-      // const index = 0;
-      // console.log('index', index);
-
-      // if (e.keyCode >= 65 && e.keyCode <= 90) {
-      //   this.notes[0].content = splice(this.notes[0].content, index, 0, e.key);
-      // } else if (e.keyCode === 8) {
-      //   console.log('DEL');
-      //   this.notes[0].content = splice(this.notes[0].content, index, 1, '');
-      // }
-      console.log('test');
-    },
-    // onEdit(e) {
-    //   const src = e.target.innerText;
-    //   this.notes[0].content = src;
-    //
-    //   console.log(parseMarkdown(src));
-    //
-    //   e.target.innerHTML = parseMarkdown(src);
-    // },
-    onBlur() {
-      console.log('blr');
-    },
     onEdit(e) {
       const currentCaretIndex = getCaretIndex(e.target);
       const content = document.getElementById('editable').innerText;
-      // console.log(document.getElementById('editable'));
-
-      console.log('edited', e.key, 'index', currentCaretIndex, 'content', content);
-
-      if (document.getElementById('editable').innerHTML.length > 4) {
-        console.log('setting caret');
-        const index = window.getSelection().focusOffset;
-        console.log('index', index, window.getSelection());
-        if (window.getSelection().isCollapsed) {
-          e.target.innerHTML = parseMarkdown(content);
-          setCaret('editable', index, 0);
+      console.log('key:', e.key, 'with keycode:', e.keyCode, 'index:', currentCaretIndex, 'content:', content);
+      if (e.keyCode >= 49 && e.keyCode <= 90) {
+        if (document.getElementById('editable').innerHTML.length > 4) {
+          console.log('SETTING caret');
+          console.log('index:', currentCaretIndex, 'selection:', window.getSelection());
+          console.log('content before parse:', content);
+          console.log('parsed', parseMarkdown(content));
+          if (window.getSelection().isCollapsed) {
+            e.target.innerHTML = parseMarkdown(content);
+            setCaret('editable', currentCaretIndex);
+          }
         }
       }
-
-      // const selection = window.getSelection();
-      // selection.extend(e.target, 2);
-
-      // const el = document.getElementByClass('editable');
-      // const selection = window.getSelection();
-      // const range = document.createRange();
-      // selection.removeAllRanges();
-      // range.selectNodeContents(el);
-      // range.collapse(false);
-      // selection.addRange(range);
-      // el.focus();
-
-      // this.$el.querySelector('.editable').blur();
-      // this.onEdit(e);
-      // this.$el.querySelector('.editable').focus();
     },
   },
 });
